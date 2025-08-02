dbf0b56964152481c4e411ece454aa88477b8c70
Hasan1817 <mahadi19961214@gmail.com>
2025-08-02 14:40:43 -0700

    Update 2021_05_23_123924_add_country_state_city_permission_to_permissions_table.php

--- database/migrations/2021_05_23_123924_add_country_state_city_permission_to_permissions_table.php
+++ database/migrations/2021_05_23_123924_add_country_state_city_permission_to_permissions_table.php
@@ -8,7 +8,7 @@ class AddCountryStateCityPermissionToPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * 
      * @return void
      */
     public function up()
